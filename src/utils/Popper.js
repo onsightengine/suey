2dfe9944a5a38643af5fb28af99e19e5969e3666
Stephens Nunnally <stevinz@gmail.com>
2022-12-17 11:49:24 -0800

    Transfer from scidian

--- src/utils/Popper.js
+++ src/utils/Popper.js
@@ -4,7 +4,7 @@
 // @about       Lightweight JavaScript UI library.
 // @author      Stephens Nunnally <@stevinz>
 // @license     MIT - Copyright (c) 2021-2022 Stephens Nunnally and Scidian Studios
-// @source      https://github.com/scidian/osui
+// @source      https://github.com/onsightengine/osui
 //
 ///////////////////////////////////////////////////////////////////////////////////*/
 //
