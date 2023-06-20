ca5b913b78355328ad5f184257476584eb0c1187
Stephens Nunnally <stevinz@gmail.com>
2023-06-20 11:13:05 -0700

    ShadowBox undefined fix

--- src/layout/ShadowBox.js
+++ src/layout/ShadowBox.js
@@ -13,7 +13,7 @@ class ShadowBox extends Div {
         if (arguments.length === 1 && Array.isArray(arguments[0])) args = arguments[0];
         for (let i = 0; i < args.length; i++) {
             let argument = args[i];
-            if (argument !== undefined && argument.isElement === true) {
+            if (argument && argument.isElement === true) {
                 this.add(argument);
             } else {
                 this.add(new VectorBox(argument));
