0f7b2fdfcb48f6c2e571990c03cd25a452d28bb7
Stephens Nunnally <stevinz@gmail.com>
2024-03-27 10:02:33 -0700

    Examples menu

---
+++ examples/imports.js
@@ -0,0 +1,14 @@
+/***** Local *****/
+
+// "suey": "../build/suey.module.js"
+// "suey": "https://unpkg.com/suey/build/suey.module.js"
+
+document.write(`
+    <script type='importmap'>
+    {
+        "imports": {
+            "suey": "https://unpkg.com/suey/build/suey.module.js"
+        }
+    }
+    </script>
+`);
